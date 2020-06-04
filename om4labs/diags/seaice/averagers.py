64edb6a46d2a6160351bed69c5841be28ee144a2
John Krasting <John.Krasting@noaa.gov>
2020-06-04 13:11:21 -0400

    Refactored averagers.py
    - Made use of python calendar module

--- om4labs/diags/seaice/averagers.py
+++ om4labs/diags/seaice/averagers.py
@@ -2,41 +2,6 @@ import calendar
 import xarray as xr
 import numpy as np
 
-dpm = {
-    "noleap": [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    "365_day": [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    "standard": [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    "gregorian": [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    "proleptic_gregorian": [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    "all_leap": [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    "366_day": [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    "360_day": [0, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30],
-}
-
-
-def leap_year(year, cal="standard"):
-    """Determine if year is a leap year"""
-    year = int(year)
-    if cal in ["standard", "gregorian", "proleptic_gregorian", "julian"]:
-        return calendar.isleap(year)
-    else:
-        return False
-
-
-def get_dpm(time, cal="standard"):
-    """
-    return a array of days per month corresponding to the months provided in `months`
-    """
-    month_length = np.zeros(len(time), dtype=np.int)
-
-    cal_days = dpm[cal]
-
-    for i, (month, year) in enumerate(zip(time.month, time.year)):
-        month_length[i] = cal_days[month]
-        if leap_year(year, cal=cal) and month == 2:
-            month_length[i] += 1
-    return month_length
-
 
 def annual_cycle(ds, var):
     """Compute annual cycle climatology"""
@@ -47,9 +12,17 @@ def annual_cycle(ds, var):
         cal = ds.time.calendar_type.lower()
     else:
         cal = "standard"
-    month_length = xr.DataArray(
-        get_dpm(ds.time.to_index(), cal=cal), coords=[ds.time], name="month_length",
-    )
+
+    if cal.lower() in ["noleap", "365day"]:
+        # always calculate days in month based on year 1 (non-leap year)
+        month_length = [calendar.monthrange(1, x.month)[1] for x in ds.time.to_index()]
+    else:
+        # use real year/month combo to calculate days in month
+        month_length = [
+            calendar.monthrange(x.year, x.month)[1] for x in ds.time.to_index()
+        ]
+
+    month_length = xr.DataArray(month_length, coords=[ds.time], name="month_length")
 
     # Calculate the weights by grouping by 'time.season'.
     # Conversion to float type ('astype(float)') only necessary for Python 2.x
