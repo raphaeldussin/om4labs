33b2ebda6faa956c8c1df042779a6edf7a546a54
Raphael Dussin <Raphael.Dussin@noaa.gov>
2020-06-11 13:02:36 -0400

    intake catalog for data, start refactor bias

--- om4labs/diags/annual_bias_1x1deg/__init__.py
+++
@@ -1,3 +0,0 @@
-from .annual_bias_1x1deg import parse_and_run
-
-__description__ = "annual bias"
--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 """ setup for om4labs """
-import setuptools
+from setuptools import setup, find_packages
+import os
+import subprocess
+import sys
 
-setuptools.setup(
+platform = 'unknown'
+if '--platform' in sys.argv:
+    index = sys.argv.index('--platform')
+    sys.argv.pop(index)  # Removes the '--platform'
+    platform = sys.argv.pop(index)  # get value of arg
+
+setup(
     name="om4labs",
     version="0.0.1",
     author="Raphael Dussin",
@@ -10,9 +19,22 @@ setuptools.setup(
     license="GPLv3",
     keywords="",
     url="https://github.com/raphaeldussin/om4labs",
-    packages=["om4labs", "om4labs/diags",
-              "om4labs/diags/annual_bias_1x1deg"],
-    scripts=[
-        "scripts/om4labs"
+    packages=find_packages(),
+    scripts=["scripts/om4labs"],
+    package_data={'': ['*.yml']},
+    data_files=[
+        ('{}/.om4labs'.format(os.environ['HOME']),
+         [os.path.join('om4labs/catalogs', 'obs_catalog_gfdl.yml'),
+          os.path.join('om4labs/catalogs', 'obs_catalog_gaea.yml'),
+          os.path.join('om4labs/catalogs', 'obs_catalog_unknown.yml')
+          ])
     ],
 )
+
+link_platform = ["ln ", "-s ",
+                 os.path.join(os.environ['HOME'], '.om4labs',
+                              f'obs_catalog_{platform}.yml'), " ",
+                 os.path.join(os.environ['HOME'], '.om4labs',
+                              'obs_catalog.yml')]
+if not os.path.exists(f"{os.environ['HOME']}/.om4labs/obs_catalog.yml"):
+    err = subprocess.check_call(''.join(link_platform), shell=True)
