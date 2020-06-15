3b1ad84f42f733b1953d4f0f18d15cac351b9ae5
Raphael Dussin <Raphael.Dussin@noaa.gov>
2020-06-15 14:45:05 -0400

    switch to pkg_resources

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 """ setup for om4labs """
 from setuptools import setup, find_packages
 import os
-import subprocess
-import sys
+
 
 is_travis = 'TRAVIS' in os.environ
-
-platform = 'unknown'
-if '--platform' in sys.argv:
-    index = sys.argv.index('--platform')
-    sys.argv.pop(index)  # Removes the '--platform'
-    platform = sys.argv.pop(index)  # get value of arg
 
 setup(
     name="om4labs",
@@ -22,23 +15,5 @@ setup(
     keywords="",
     url="https://github.com/raphaeldussin/om4labs",
     packages=find_packages(),
-    scripts=["scripts/om4labs"],
-    package_data={'': ['*.yml']},
-    data_files=[
-        ('{}/.om4labs'.format(os.environ['HOME']),
-         [os.path.join('om4labs/catalogs', 'obs_catalog_gfdl.yml'),
-          os.path.join('om4labs/catalogs', 'obs_catalog_gaea.yml'),
-          os.path.join('om4labs/catalogs', 'obs_catalog_unknown.yml')
-          ])
-    ],
+    scripts=["scripts/om4labs"]
 )
-
-link_platform = ["ln ", "-s ",
-                 os.path.join(os.environ['HOME'], '.om4labs',
-                              f'obs_catalog_{platform}.yml'), " ",
-                 os.path.join(os.environ['HOME'], '.om4labs',
-                              'obs_catalog.yml')]
-
-if not os.path.exists(f"{os.environ['HOME']}/.om4labs/obs_catalog.yml"):
-    if not is_travis:
-        err = subprocess.check_call(''.join(link_platform), shell=True)
