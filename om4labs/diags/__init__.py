b3bb1da9a5a7f5462a7f848f32cef2fe8b8a59f6
Raphael Dussin <Raphael.Dussin@noaa.gov>
2020-06-16 13:28:47 -0400

    start splitting bias

--- om4labs/diags/__init__.py
+++ om4labs/diags/__init__.py
@@ -1,1 +1,2 @@
 from . import generic_annual_bias_1x1deg
+from . import sst_annual_bias_1x1deg
---
+++ om4labs/diags/sst_annual_bias_1x1deg/__init__.py
@@ -0,0 +1,3 @@
+from .sst_annual_bias_1x1deg import parse_and_run
+
+__description__ = "SST annual bias plot"
