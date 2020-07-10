263be0b18abb3f12fe30a5a64beeb55d6abc292c
John Krasting <John.Krasting@noaa.gov>
2020-07-10 10:32:36 -0400

    import MaxNLocator in chooseColorLevels.py
    - added line:
      from matplotlib.ticker import MaxNLocator

--- om4labs/m6plot/cm/chooseColorLevels.py
+++ om4labs/m6plot/cm/chooseColorLevels.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib.colors import BoundaryNorm, ListedColormap, LogNorm
-
+from matplotlib.ticker import MaxNLocator
 
 def chooseColorLevels(
     sMin,
