21f6abd4b2a52e188caf68b80e79df92649d7594
John Krasting <John.Krasting@noaa.gov>
2020-07-15 11:52:38 -0400

    Black formatting for sst/sss __init__.py files

--- om4labs/diags/sss_annual_bias_1x1deg/__init__.py
+++ om4labs/diags/sss_annual_bias_1x1deg/__init__.py
@@ -1,4 +1,4 @@
 from .sss_annual_bias_1x1deg import parse_and_run, parse, run
 
 __description__ = "SSS annual bias plot"
-__ppstreams__ = ["ocean_annual_z_d2_1x1deg/av","ocean_annual_z_1x1deg/av"]
+__ppstreams__ = ["ocean_annual_z_d2_1x1deg/av", "ocean_annual_z_1x1deg/av"]
--- om4labs/diags/sst_annual_bias_1x1deg/__init__.py
+++ om4labs/diags/sst_annual_bias_1x1deg/__init__.py
@@ -1,4 +1,4 @@
 from .sst_annual_bias_1x1deg import parse_and_run, parse, run
 
 __description__ = "SST annual bias plot"
-__ppstreams__ = ["ocean_annual_z_d2_1x1deg/av","ocean_annual_z_1x1deg/av"]
+__ppstreams__ = ["ocean_annual_z_d2_1x1deg/av", "ocean_annual_z_1x1deg/av"]
