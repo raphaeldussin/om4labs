c014c6debd1503bd645dcd621210c7cfd83d58f8
John Krasting <John.Krasting@noaa.gov>
2020-07-15 15:17:10 -0400

    Enforced an interactive mpl backend (for now)

--- om4labs/m6plot/xyplot.py
+++ om4labs/m6plot/xyplot.py
@@ -74,6 +74,8 @@ def xyplot(
     # set visual backend
     if interactive is False:
         plt.switch_backend("Agg")
+    else:
+        plt.switch_backend("qt5agg")
 
     c = cm.dunne_pm()
     c = cm.dunne_rainbow()
