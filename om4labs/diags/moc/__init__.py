f29ca07e058a6fd634666f5e88c053c237350f5c
John Krasting <John.Krasting@noaa.gov>
2020-07-23 16:24:36 -0400

    Updated list of acceptable ppdirs for moc script

--- om4labs/diags/moc/__init__.py
+++ om4labs/diags/moc/__init__.py
@@ -1,5 +1,5 @@
 from .moc import parse, read, calculate, plot, run, parse_and_run
 
 __description__ = "Plots global and basin-average overturning streamfunction"
-__ppstreams__ = ["ocean_month_z_d2_refined/av", "ocean_monthly_z_d2/ts"]
+__ppstreams__ = ["ocean_month_z_d2_refined/av", "ocean_monthly_z_d2/ts","ocean_annual_z/av"]
 __ppvars__ = ["vmo"]
