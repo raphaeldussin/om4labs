52101d39588b671b08d327c90e0988edbbb65281
John Krasting <John.Krasting@noaa.gov>
2020-08-21 15:46:14 -0400

    Updated om4labs driver to recognize heat transport diag

--- om4labs/diags/__init__.py
+++ om4labs/diags/__init__.py
@@ -1,5 +1,6 @@
 from . import avail
 from . import generic_annual_bias_1x1deg
+from . import heat_transport
 from . import moc
 from . import sst_annual_bias_1x1deg
 from . import sss_annual_bias_1x1deg
