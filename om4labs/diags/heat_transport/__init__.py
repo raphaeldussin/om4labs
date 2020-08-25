3a153e6689ab4134bd61624439c20d856713f3de
John Krasting <John.Krasting@noaa.gov>
2020-08-25 11:47:27 -0400

    Updated default pp dir for heat transport diag

--- om4labs/diags/heat_transport/__init__.py
+++ om4labs/diags/heat_transport/__init__.py
@@ -2,8 +2,6 @@ from .heat_transport import parse, read, calculate, plot, run, parse_and_run
 
 __description__ = "Plots global and basin-average poleward heat transport"
 __ppstreams__ = [
-    "ocean_month_z_d2_refined/av",
-    "ocean_monthly_z_d2/ts",
-    "ocean_annual_z/av",
+    "ocean_monthly/av",
 ]
-__ppvars__ = ["vmo"]
+__ppvars__ = ["T_ady_2d"]
