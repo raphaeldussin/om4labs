9d1e2a5bf94584ced389683505d63c9e815c72ac
John Krasting <John.Krasting@noaa.gov>
2020-09-28 17:00:33 -0400

    Added OM4labs plotting routines for yz plots

--- om4labs/om4plotting.py
+++ om4labs/om4plotting.py
@@ -30,3 +30,29 @@ def plot_xycompare(
     )
 
     return fig
+
+
+def plot_yzdiff(y, z, slice1, slice2, diff_kwargs, interactive=False, stream=False):
+    """ make difference plot """
+    if stream:
+        img = io.BytesIO()
+        diff_kwargs["save"] = img
+
+    fig = m6plot.yzplot(slice1 - slice2, y, z, interactive=interactive, **diff_kwargs)
+
+    return fig
+
+
+def plot_yzcompare(
+    y, z, slice1, slice2, compare_kwargs, interactive=False, stream=False
+):
+    """ make 3 panel compare plot """
+    if stream:
+        img = io.BytesIO()
+        compare_kwargs["save"] = img
+
+    fig = m6plot.yzcompare(
+        slice1, slice2, y, z, interactive=interactive, **compare_kwargs
+    )
+
+    return fig
