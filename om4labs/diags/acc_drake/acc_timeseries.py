d43987f09b51eaaaa5f2ed78c6b165480d0c5528
Rebecca Beadling <Rebecca.Beadling@ldt-4323515.gfdl.noaa.gov>
2021-08-17 13:38:18 -0400

    Initial directory for ACC throught Drake diagnostic

---
+++ om4labs/diags/acc_drake/acc_timeseries.py
@@ -0,0 +1,22 @@
+def parse():
+    return
+
+
+def read():
+    return
+
+
+def calculate():
+    return
+
+
+def plot():
+    return
+
+
+def run():
+    return
+
+
+def parse_and_run():
+    return
