ee53469ed6e7dac6edeedc6721808b13af6d9259
Brandon Reichl <Brandon.Reichl@noaa.gov>
2021-10-27 09:50:55 -0400

    Sort import statements in diags/__init__.py

--- om4labs/diags/__init__.py
+++ om4labs/diags/__init__.py
@@ -3,6 +3,7 @@ from . import generic_annual_bias_1x1deg
 from . import generic_section_transport
 from . import generic_yz_annual_bias_1x1deg
 from . import heat_transport
+from . import mld
 from . import moc
 from . import sst_annual_bias_1x1deg
 from . import sss_annual_bias_1x1deg
@@ -10,4 +11,3 @@ from . import seaice
 from . import thetao_yz_annual_bias_1x1deg
 from . import section_transports
 from . import so_yz_annual_bias_1x1deg
-from . import mld
