ceb0a75d6f8fcd8bc13984d851d3b3bcebce0704
John Krasting <John.Krasting@noaa.gov>
2023-10-24 16:23:25 -0400

    Dynamic import of diagnostics
    - Code will search the `diags` directory for diagnostics
      to import. If the diagnostic fails to import, it will give a
      warning but not block the remaining diagnostics from being
      imported.

--- src/omlabs/diags/__init__.py
+++ src/omlabs/diags/__init__.py
@@ -1,20 +1,23 @@
-from . import avail
-from . import enso
-from . import depth_time_temperature_drift
-from . import depth_time_salinity_drift
-from . import generic_annual_bias_1x1deg
-from . import generic_depth_time_drift
-from . import generic_section_transport
-from . import generic_yz_annual_bias_1x1deg
-from . import heat_transport
-from . import moc
-from . import sst_annual_bias_1x1deg
-from . import sss_annual_bias_1x1deg
-from . import seaice
-from . import stratification
-from . import stress_curl
-from . import thetao_yz_annual_bias_1x1deg
-from . import section_transports
-from . import so_yz_annual_bias_1x1deg
-from . import acc_drake
-from . import surface_wmt
+import os
+import warnings
+
+_diag_dir = os.path.split(os.path.abspath(__file__))[0]
+_exclude_list = ["__pycache__"]
+
+_diaglist = [_x for _x in os.listdir(_diag_dir)]
+_diaglist = [f"{_diag_dir}/{_x}" for _x in _diaglist]
+_diaglist = [_x for _x in _diaglist if os.path.isdir(_x) and os.path.basename(_x) not in _exclude_list]
+_diaglist = sorted([os.path.basename(_x) for _x in _diaglist])
+
+for _diag in _diaglist:
+    try:
+        exec(f"from . import {_diag}")
+    except Exception as exc:
+        warnings.warn(f"Unable to import diagnostic: {_diag}")
+
+del os
+del warnings
+del _diag
+del _diaglist
+del _diag_dir
+del _exclude_list
